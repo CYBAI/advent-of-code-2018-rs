0e3922a6cdd4599481f83d9691f30d6bbecad2d3
CYBAI <cyb.ai.815@gmail.com>
2018-12-06 02:08:50 +0800

    Move `fold_polymer` as shared method

--- day05/src/lib.rs
+++ day05/src/lib.rs
@@ -1,1 +1,18 @@
 extern crate aoc_utils;
+
+pub fn fold_polymer(mut acc: Vec<char>, c: char) -> Vec<char> {
+    if let Some(last_char) = acc.pop() {
+        if (last_char.is_lowercase() && c.is_uppercase()
+            || last_char.is_uppercase() && c.is_lowercase())
+            && (last_char.to_lowercase().to_string() == c.to_lowercase().to_string())
+        {
+            return acc;
+        }
+
+        acc.push(last_char);
+    }
+
+    acc.push(c);
+
+    acc
+}
--- day05/src/part1/main.rs
+++ day05/src/part1/main.rs
@@ -1,26 +1,12 @@
 use aoc_utils::read_file;
+use day05::fold_polymer;
 
 fn main() {
     if let Ok(contents) = read_file("./input") {
         let result: i32 = contents
             .split("")
             .filter_map(|s| if s == "" { None } else { s.chars().next() })
-            .fold(Vec::new(), |mut acc: Vec<char>, c| {
-                if let Some(last_char) = acc.pop() {
-                    if (last_char.is_lowercase() && c.is_uppercase()
-                        || last_char.is_uppercase() && c.is_lowercase())
-                        && (last_char.to_lowercase().to_string() == c.to_lowercase().to_string())
-                    {
-                        return acc;
-                    }
-
-                    acc.push(last_char);
-                }
-
-                acc.push(c);
-
-                acc
-            })
+            .fold(Vec::new(), fold_polymer)
             .len() as i32;
 
         println!("{:?}", result);
