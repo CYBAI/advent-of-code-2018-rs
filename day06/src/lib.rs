90ea94ba4bcfaafe48e946340f61f0c101ce26cd
CYBAI <cyb.ai.815@gmail.com>
2018-12-09 21:39:42 +0800

    Complete day 6 part 2

--- day06/src/lib.rs
+++ day06/src/lib.rs
@@ -9,3 +9,8 @@ use regex::Regex;
 lazy_static! {
     pub static ref COORDINATE_REGEX: Regex = Regex::new(r"(?P<x>\d+), (?P<y>\d+)").unwrap();
 }
+
+/// https://en.wikipedia.org/wiki/Taxicab_geometry#Formal_definition
+pub fn find_manhattan_distance((x1, y1): (i32, i32), (x2, y2): (i32, i32)) -> i32 {
+    (x1 - x2).abs() + (y1 - y2).abs()
+}
--- day06/src/part2/main.rs
+++ day06/src/part2/main.rs
@@ -1,9 +1,10 @@
 use aoc_utils::read_file;
 use day06::COORDINATE_REGEX;
 use day06::find_manhattan_distance;
-use std::collections::HashMap;
 
 fn main() {
+    let maximum = 10000;
+
     if let Ok(contents) = read_file("./input") {
         let coordinates = contents
             .split('\n')
@@ -34,7 +35,7 @@ fn main() {
             None => return,
         };
 
-        let mut hash: HashMap<i32, (i32, bool)> = HashMap::new();
+        let mut counter = 0;
 
         for i in min_x..=max_x {
             for j in min_y..=max_y {
@@ -43,35 +44,14 @@ fn main() {
                     .map(|c| find_manhattan_distance(*c, (i, j)))
                     .collect::<Vec<i32>>();
 
-                let min = distances.iter().min().unwrap();
+                let sum: i32 = distances.iter().sum();
 
-                let mins = distances
-                    .iter()
-                    .enumerate()
-                    .filter_map(|(i, n)| if n == min { Some(i as i32) } else { None })
-                    .collect::<Vec<i32>>();
-
-                if mins.len() == 1 {
-                    let on_x_bound = i == min_x || i == max_x;
-                    let on_y_bound = j == min_y || j == max_y;
-
-                    let (counter, on_bound) = hash.entry(mins[0]).or_insert((0, false));
-
-                    if !*on_bound && (on_x_bound || on_y_bound) {
-                        *on_bound = true;
-                    }
-
-                    *counter += 1;
+                if sum < maximum {
+                    counter += 1;
                 }
             }
         }
 
-        let result = hash
-            .iter()
-            .filter(|(_, (_, on_bound))| !on_bound)
-            .max_by_key(|(_, (counter, _))| counter)
-            .unwrap();
-
-        println!("{:?}", (result.1).0);
+        println!("{:?}", counter);
     }
 }
